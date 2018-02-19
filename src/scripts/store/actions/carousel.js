3ce33ceb43747368044482a5b6dc03ad5c4c700b
mikkelmoerch <mikkel.moerch2@live.dk>
2018-02-19 21:21:11 +0100

    remove unused code

--- src/scripts/store/actions/carousel.js
+++ src/scripts/store/actions/carousel.js
@@ -1,7 +1,6 @@
 import {
   ADD_SLIDE,
   NEXT_SLIDE,
-  PREV_SLIDE,
   START_LOOP,
   NEXT_LOOP_SLIDE,
   PAUSE_PLAYER,
@@ -10,7 +9,6 @@ import {
 
 export const addSlide = payload => { type: ADD_SLIDE, payload }
 export const next = () => { type: NEXT_SLIDE }
-export const prev = () => { type: PREV_SLIDE }
 export const startLoop = payload => { type: START_LOOP, payload }
 export const nextLoopSlide = () => { type: NEXT_LOOP_SLIDE }
 export const pausePlayer = () => { type: PAUSE_PLAYER }
